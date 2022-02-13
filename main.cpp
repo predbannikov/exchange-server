ccafa5a0870a4c71b03ffb74df16d53e922374db
Yurok <predbannikov.yurii@gmail.com>
2022-02-13 23:24:47 +0300

    save state

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include "wss.h"
 
-
-int main()
+int main(int argc, char* argv[])
 {
-    return 0;
+    if (argc != 4)
+    {
+        std::cerr <<
+            "WSS Version " << wsslib_VERSION_MAJOR << "." << wsslib_VERSION_MINOR << std::endl <<
+            "Usage: websocket-server-async <address> <port> <threads>\n" <<
+            "Example:\n" <<
+            "    websocket-server-async 0.0.0.0 8080 1\n";
+        return EXIT_FAILURE;
+    }
+    const auto address = boost::asio::ip::make_address(argv[1]);
+    const auto port = static_cast<unsigned short>(std::atoi(argv[2]));
+    const auto threads = std::max<int>(1, std::atoi(argv[3]));
+    boost::asio::io_context ioc{threads};
+
+    boost::make_shared<exchange::listener>(ioc, boost::asio::ip::tcp::endpoint(address, port))->run();
+
+    std::vector<std::thread> v;
+    v.reserve(threads);
+    for(auto i = threads - 1; i > 0; --i)
+        v.emplace_back(
+            [&ioc]{
+                ioc.run();
+            });
+    ioc.run();
+    return EXIT_SUCCESS;
 }
